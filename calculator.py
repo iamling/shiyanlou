3b857a7514983ce6711d0ca3ce106293d56fa29a
1306536844 <1306536844@qq.com>
2017-10-22 23:13:42 +0800

    again

--- calculator.py
+++ calculator.py
@@ -1,6 +1,46 @@
 #!/usr/bin/env python3
-def ss(money):
-	t=money*(0.08+0.02+0.005+0.06)
+
+class config(object):
+	def __init__(self,configfile):
+		filename = configfile
+		self._config={}
+		with open(filename) as file:
+			L=file.readlines()
+		for i in range(1,len(L)+1):
+			self._config[L[i-1].split()[0].strip()]=float(L[i-1].split()[2])
+	def get_config(self,key):
+		return self._config[key]
+
+class UserData(object):
+	def __init__(self,userdatafile):
+		filename=userdatafile
+		self.userdata={}
+		with open(filename) as file:
+			L=file.readlines()
+		for i in range(1,len(L)+1):
+			self.userdata[int(L[i-1].split(',')[0])]=float(L[i-1].split(',')[1])
+	def calculator(self):
+		self.shebaolist={}
+		self.taxlist={}
+		self.salarylist={}
+		for i in self.userdata.keys():
+			self.shebaolist[i]=format(ss(self.userdata[i],L._config),".2f")
+			self.taxlist[i]=format(tax(self.userdata[i]-float(self.shebaolist[i])-3500),".2f")
+			self.salarylist[i]=format(self.userdata[i]-float(self.shebaolist[i])-float(self.taxlist[i]),".2f")
+	def dumptofile(self,outputfile):
+		filename=outputfile
+		with open(filename,'w') as file:
+			file.truncate()
+		for i in self.userdata.keys():
+			with open(filename,'a') as file:
+				file.write(str(i)+','+str(self.userdata[i])+','+str(self.shebaolist[i])+','+str(self.taxlist[i])+','+str(self.salarylist[i])+'\n')
+
+def ss(money,L):
+	if money<L['JiShuL']:
+		money=L['JiShuL']
+	elif money>L['JiShuH']:
+		money=L['JiShuH']
+	t=money*(L['Yanglao']+L['YiLiao']+L['ShiYe']+L['GongShang']+L['ShengYu']+L['GongJiJin'])
 	return t
 
 def tax(t):
@@ -24,19 +64,18 @@ def tax(t):
 	return m	
 
 import sys
-for i in range(1,len(sys.argv)):
-	L=sys.argv[i].split(':')
-	try:
-		s=int(L[0])
-		t=int(L[1])
-	except:
-		print("Parameter Error")
-	
-	m = ss(t)
-	n = tax(t-m-3500)
-	y=t-m-n
-	y=format(y,".2f")
-	v=str(s)
-	u=str(y)
-	print(v+":"+u)
+try:
+	args = sys.argv[1:]
+	index = args.index('-c')
+	configfile=args[index+1]
+	index = args.index('-d')
+	userdatafile=args[index+1]
+	index = args.index('-o')
+	outputfile=args[index+1]
+except:
+	print("error,don't exit")
 
+L = config (configfile)
+LL= UserData(userdatafile)
+LL.calculator()
+LL.dumptofile(outputfile)
