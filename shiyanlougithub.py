e3a96841fb2b7299ea751fb50fc0cc5a6496e7a1
1306536844 <1306536844@qq.com>
2017-11-12 14:44:32 +0800

    try

--- shiyanlougithub.py
+++ shiyanlougithub.py
@@ -1,16 +1,17 @@
+#! /usr/bin/env python3
+# -*- coding:utf-8 -*-
 import scrapy
 class shiyanlougithub(scrapy.Spider):
     name='shiyanlou_github'
     def start_requests(self):
         url_tmpl='https://github.com/shiyanlou?tab=repositories'
-        urls=(url_tmpl.format(i) for i in range(1,109))
+        urls=(url_tmpl.format(i) for i in range(1,5))
         for url in urls:
             yield scrapy.Request(url=url,callback=self.parse)
 
     def parse(self,response):
-        for re in response.css('div#user-repositories-list li'):
+        for re in response.css('div#user-repositories-list li.public'):
             yield{
-                    'name':re.css('h3 a::text,span.mr-3::text').re('\s\w+\-?\w*\-?\w+'),
-                    'update_time':re.css('relative-time::attr(datetime)').extract()
+                    'name':re.css('h3 a::text').re_first("\s\w+\-?\w*\-?\w+"),
+                    'update_time': re.xpath('.//relative-time/@datetime').extract_first()
                     }
-            pass
