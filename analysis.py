f4fc7e6d37dfcf8ad27ab7092d1a450ec4e7a695
1306536844 <1306536844@qq.com>
2017-11-23 18:22:52 +0800

    try2

--- analysis.py
+++ analysis.py
@@ -1,4 +1,5 @@
 import json
+import numpy as np
 import pandas as pd
 import sys
 import types
@@ -11,7 +12,25 @@ def analysis(file,user_id):
         #json_str=file.readlines()
     filename='/home/shiyanlou/Code/shiyanlou/'+file
     diclist=[json.loads(line) for line in open(filename)]
-    print(type(diclist))
+    
+    dict={}
+    l1=[]
+    l2=[]
+    l3=[]
+    l4=[]
+    l5=[]
+    for i in diclist:
+        for j in i:
+            l1.append(j['minutes'])
+            l2.append(j['created_at'])
+            l3.append(j['user_id'])
+            l4.append(j['lab'])
+            l5.append(j['course'])
+
+    a1=np.array([l1,l2,l3,l4,l5])
+    print(a1)
+    print(l4)
+    #print(dict['minutes'])
 
 if __name__=='__main__':
     analysis(sys.argv[1],sys.argv[2])
