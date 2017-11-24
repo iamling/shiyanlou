07af9c2fe4735bce100ea30be4e425a8494c3687
1306536844 <1306536844@qq.com>
2017-11-24 23:35:16 +0800

    try3

--- analysis.py
+++ analysis.py
@@ -2,14 +2,11 @@ import json
 import numpy as np
 import pandas as pd
 import sys
-import types
 
 def analysis(file,user_id):
     times = 0
     minutes = 0
 
-    #with open('/home/shiyanlou/Code/shiyanlou/'+file,'r') as file:
-        #json_str=file.readlines()
     filename='/home/shiyanlou/Code/shiyanlou/'+file
     diclist=[json.loads(line) for line in open(filename)]
     
@@ -27,10 +24,17 @@ def analysis(file,user_id):
             l4.append(j['lab'])
             l5.append(j['course'])
 
-    a1=np.array([l1,l2,l3,l4,l5])
-    print(a1)
-    print(l4)
-    #print(dict['minutes'])
+    df=pd.DataFrame({'minutes':l1,
+        'created_at':l2,
+        'user_id':l3,
+        'lab':l4,
+        'course':l5})
+    df1=df['minutes'].groupby(df['user_id'])
+    times=df1.count()[int(user_id)]
+    minutes = df1.mean()[int(user_id)]*times
+
+
+    return times,minutes
 
 if __name__=='__main__':
     analysis(sys.argv[1],sys.argv[2])
